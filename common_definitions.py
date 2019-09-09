71772a4ff2c830317f0a037006b244a3af44ee34
radoxpi <samuelmat19@gmail.com>
2019-09-09 03:49:16 +0200

    Fixed saving gt in evaluation

--- common_definitions.py
+++ common_definitions.py
@@ -50,6 +50,7 @@ MOBILENETV2_WEIGHT_PATH = "model_weights/pix2code_MobileNetV2.h5"  # autoencoder
 TRANSFORMER_WEIGHT_PATH = "model_weights/pix2code_transformer.h5"  # transformer trained on pix2code datasets
 
 AUTOENCODER_CHECKPOINT_PATH = "./checkpoints/train/autoencoder"
+# TRANSFORMER_CHECKPOINT_PATH = "./checkpoints/train/transformer_transfer_learning"  # this has good result!
 TRANSFORMER_CHECKPOINT_PATH = "./checkpoints/train/transformer_transfer_learning"
 # DATASET_CACHE = "/tmp/img2source_dataset_cache"
 

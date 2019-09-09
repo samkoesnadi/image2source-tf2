71df66dd361c27e9f19db7a0d9cf414759c18c65
radoxpi <samuelmat19@gmail.com>
2019-09-09 14:16:52 +0200

    Changed configuration for new parsed dataset

--- common_definitions.py
+++ common_definitions.py
@@ -21,7 +21,7 @@ from PIL import Image
 import pickle
 
 TOP_K = 20000  # this is for tokenizer
-MAX_SEQ_LEN = 841
+MAX_SEQ_LEN = 1050
 
 ACTIVATION = tf.nn.leaky_relu
 KERNEL_INITIALIZER = tf.keras.initializers.he_normal()
@@ -29,7 +29,7 @@ KERNEL_INITIALIZER = tf.keras.initializers.he_normal()
 ### Set default parameters for all model
 IMAGE_INPUT_SIZE = 224  # this to fit default criteria from MobileNetV2
 BATCH_SIZE_AUTOENCODER = 64
-BATCH_SIZE = 13
+BATCH_SIZE = 10
 BUFFER_SIZE = 500  # this is important for shuffling
 EPOCHS = 1000
 DROPOUT_RATE = 0.1
@@ -41,18 +41,29 @@ DEFAULT_LEARNING_RATE = 1e-3
 IS_TRAINING = True
 TRANSFER_LEARN_AUTOENCODER = True
 
-TFRECORD_FILENAME = "datasets/pix2code.tfrecord"
-TOKENIZER_FILENAME = "datasets/pix2code_tokenizer.json"
-ADDITIONAL_FILENAME = "datasets/pix2code_additional.json"
 ANNOTATIONS_PATH = "annotations.json"
+
+# Normal parser
+# TFRECORD_FILENAME = "datasets/pix2code.tfrecord"  # this has good result!
+# TOKENIZER_FILENAME = "datasets/pix2code_tokenizer.json"  # this has good result!
+# ADDITIONAL_FILENAME = "datasets/pix2code_additional.json"
+
+# Harder parser
+TFRECORD_FILENAME = "datasets/pix2code_harder.tfrecord"
+TOKENIZER_FILENAME = "datasets/pix2code_tokenizer_harder.json"
+ADDITIONAL_FILENAME = "datasets/pix2code_additional_harder.json"
 
 MOBILENETV2_WEIGHT_PATH = "model_weights/pix2code_MobileNetV2.h5"  # autoencoder trained on pix2code datasets
 TRANSFORMER_WEIGHT_PATH = "model_weights/pix2code_transformer.h5"  # transformer trained on pix2code datasets
 
 AUTOENCODER_CHECKPOINT_PATH = "./checkpoints/train/autoencoder"
+
+# Normal parser
 # TRANSFORMER_CHECKPOINT_PATH = "./checkpoints/train/transformer_transfer_learning"  # this has good result!
-TRANSFORMER_CHECKPOINT_PATH = "./checkpoints/train/transformer_transfer_learning"
-# DATASET_CACHE = "/tmp/img2source_dataset_cache"
+
+# Harder parser
+TRANSFORMER_CHECKPOINT_PATH = "./checkpoints/train/transformer_transfer_learning_harder"
+
 
 ### Set Hyperparameters for Transformer
 num_layers = 6

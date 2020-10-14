34cf5fefaadb369d43db1df1dc9e75f5419a2e34
radoxpi <samuelmat19@gmail.com>
2020-10-14 03:23:18 +0200

    Updated README.md and fixed small bugs

--- experiment.py
+++ experiment.py
@@ -36,9 +36,30 @@ print(list(map(lambda i: tokenizer.index_word[i], train_seqs[0])))
 print(tokenizer.sequences_to_texts(train_seqs))
 print(decode_2_html(tokenizer.sequences_to_texts(train_seqs)[0]))
 
-temp_learning_rate_schedule = CustomSchedule(d_model, WARM_UP_STEPS)
+class SCSTCustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):  # this one will go down second time some how
+	def __init__(self, max_temperature, min_temperature):
+		super(SCSTCustomSchedule, self).__init__()
 
+		if max_temperature < min_temperature:
+			raise Exception("max temperature is less than min temperature")
+
+		self.max_temperature = max_temperature
+		self.min_temperature = min_temperature
+
+		self.multiplier = 10 ** -4.7 if self.max_temperature >= 1 else 10 ** 1.6
+
+
+	def __call__(self, step):
+		return tf.maximum(self.min_temperature, self.max_temperature * tf.exp(-self.max_temperature * self.multiplier * step))
+
+temp_learning_rate_schedule = CustomSchedule(dff, WARM_UP_STEPS, .3)
+# a = tf.random.normal([100])
+# a = tf.nn.softmax(a/5)
+# temp_learning_rate_schedule = SCSTCustomSchedule(5e-6, 5e-7)
+# temp_learning_rate_schedule = lambda t : tf.minimum(1e-4*t, 3e-4)
+print(tf.reduce_max(temp_learning_rate_schedule(tf.range(40000, dtype=tf.float32))))
 plt.plot(temp_learning_rate_schedule(tf.range(40000, dtype=tf.float32)))
+# plt.plot(a)
 plt.ylabel("Learning Rate")
 plt.xlabel("Train Step")
 plt.show()
